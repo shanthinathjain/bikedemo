f0a52931a89619ad82f898dccce8c00319334c69
shantinathjain <snjain16@gmail.com>
2022-02-16 17:30:15 +0530

    changed title for read php

--- read.php
+++ read.php
@@ -54,7 +54,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>View Record</title>
+    <title>View Car Records</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper{
